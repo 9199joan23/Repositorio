6040c87153b4aeb9412a7726034b6cc736cecaf7
usuari <usuari>
2016-09-26 15:34:49 +0200

    ejercicio 1 de php david

---
+++ 19092016_ex1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>  Página PHP </title>
+</Head>
+<body>
+
+<?php
+echo "<h1>hola</h1>";
+
+echo "<br> <br>";
+
+
+for ($cont=1;$cont<=10;$cont++){
+	echo "cont: $cont<br>";
+}
+?>
+</body>
+</html>
